72c2901328746782125bb8f2a9e5723df73b4d29
lhannachi <lhannachi@vermeg.com>
2021-06-26 18:58:47 +0100

    create project

---
+++ src/main/java/com/esprit/weBank/WebankApplication.java
@@ -0,0 +1,13 @@
+package com.esprit.weBank;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebankApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WebankApplication.class, args);
+	}
+
+}
