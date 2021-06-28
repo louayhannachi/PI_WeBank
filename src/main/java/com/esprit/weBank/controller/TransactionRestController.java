d121f04a0408e323dbce9f3af121050d6ab87402
aymenguediche <aymen.guediche@esprit.tn>
2021-06-28 22:23:19 +0100

    add some crud  api

--- src/main/java/com/esprit/weBank/controller/TransactionRestController.java
+++ src/main/java/com/esprit/weBank/controller/TransactionRestController.java
@@ -3,24 +3,48 @@ package com.esprit.weBank.controller;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 import com.esprit.weBank.entities.Transaction;
+import com.esprit.weBank.entities.User;
 import com.esprit.weBank.services.TransactionService;
 
 @RestController
 public class TransactionRestController {
 	@Autowired
 	private TransactionService transactionService;	
+
+	@PutMapping(value = "/createTransaction")
+    public Transaction createTransaction(@RequestBody Transaction transaction) {
+		return transactionService.addTransaction(transaction);
+	}
+	@GetMapping(value ="/getAllTransactions")
+	public List<Transaction> getAllTransactions() {
+		return transactionService.findAllTransaction();
+	}	
 	
+
+	@DeleteMapping(value ="/deleteTransaction/{id}")
+	public void deleteTransaction(@PathVariable(value = "id") int id) {
+		transactionService.deleteTransactionById(id);
+	}
 	@GetMapping(value ="/getTransactionByRibE/{ribE}")
 	public List<Transaction> findTransactionByRibE(@PathVariable(value = "ribE") int ribE) {
 		return transactionService.findTransactionByRibE(ribE);
 	}
 	
-	@GetMapping(value ="/getAllTransactions")
-	public List<Transaction> getAllTransactions() {
-		return transactionService.findAllTransaction();
+	@GetMapping(value ="/getTransactionsById/{id}")
+	public Transaction getTransactionById(@PathVariable(value = "id") int id) {
+		return transactionService.findTransactionById(id);
+}
+	
+	@PostMapping(value ="/updateTransaction/{id}")
+	public Transaction updateTransaction(@PathVariable(value = "id") int id, @RequestBody Transaction transaction) {
+		return transactionService.updateTransaction(transaction, id);
 	}
 }
--- src/main/java/com/esprit/weBank/services/TransactionService.java
+++ src/main/java/com/esprit/weBank/services/TransactionService.java
@@ -4,13 +4,17 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import com.esprit.weBank.entities.Account;
 import com.esprit.weBank.entities.Transaction;
+import com.esprit.weBank.entities.User;
 import com.esprit.weBank.repository.ITransactionRepository;
 
 @Service
 public class TransactionService {
 	@Autowired
 	private ITransactionRepository transactionRepository;
+	
 	
 	public List<Transaction> findAllTransaction() {
 		return (List<Transaction>) transactionRepository.findAll();
@@ -19,5 +23,33 @@ public class TransactionService {
 		
 		return (List<Transaction>) transactionRepository.findByRibE(ribE).orElse(null);
 	}
+	
+	 public Transaction addTransaction(Transaction transaction) {
+	        return transactionRepository.save(transaction);
+	    }
+	
+	 public Transaction findTransactionById(int id) {
+			return transactionRepository.findById(id).orElse(null);
+		}
+	 
+	 
+	 public Transaction updateTransaction(Transaction transaction, int id) {
+		 Transaction existingTransaction = findTransactionById(id);
+			if (existingTransaction != null) {
+				existingTransaction.setNomC(transaction.getNomC());
+				existingTransaction.setType(transaction.getType());
+				existingTransaction.setRibE(transaction.getRibE());
+				existingTransaction.setRibR(transaction.getRibR());
+				existingTransaction.setDateT(transaction.getDateT());
+				existingTransaction.setMontant(transaction.getMontant());
+				existingTransaction.setdevise(transaction.getdevise());
+				return transactionRepository.save(existingTransaction);
+			}		
+			return null;
+		}
+	 
+	 public void deleteTransactionById(int id) {
+		 transactionRepository.deleteById(id);
+		}
 	
 }
